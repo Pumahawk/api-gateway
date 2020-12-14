537f3d0049b7e698cbb177111aa3019e5629ced2
Lorenzo Gandino <lorenzoga94@gmail.com>
2020-12-14 20:14:38 +0100

    init

---
+++ src/main/java/com/github/pumahawk/apigateway/apigatewaycore/ApigatewayCoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.pumahawk.apigateway.apigatewaycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayCoreApplication.class, args);
+	}
+
+}
