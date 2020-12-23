762f54a3283eea4d6f77118cabbdfb840bf078f9
Lorenzo Gandino <lorenzoga94@gmail.com>
2020-12-23 03:40:09 +0100

    add implementazione CloneSessionSolver

---
+++ src/main/java/com/github/pumahawk/apigateway/apigatewaycore/configurations/resolvers/clonesession/CloneSessionConfiguration.java
@@ -0,0 +1,70 @@
+
+package com.github.pumahawk.apigateway.apigatewaycore.configurations.resolvers.clonesession;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "type",
+    "uri",
+    "guestToken"
+})
+public class CloneSessionConfiguration {
+
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("uri")
+    private String uri;
+    @JsonProperty("guestToken")
+    private GuestToken guestToken;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("uri")
+    public String getUri() {
+        return uri;
+    }
+
+    @JsonProperty("uri")
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    @JsonProperty("guestToken")
+    public GuestToken getGuestToken() {
+        return guestToken;
+    }
+
+    @JsonProperty("guestToken")
+    public void setGuestToken(GuestToken guestToken) {
+        this.guestToken = guestToken;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
